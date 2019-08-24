a1aec9aef44a033a3c734c5d0a695467c94ceae8
awaluwalakhiru <awalprasetyo4@gmail.com>
2019-08-24 18:44:20 +0700

    menambah efek border bottom css saat hover

--- js/home.js
+++ js/home.js
@@ -1,11 +1,8 @@
 $('.card').hide();
 $('nav.load').hide();
-$(function()
-{
-    $('.card').each(function(i)
-    {
-        $(this).delay(500 * (i + 1)).slideDown(500 * (1 + i), function()
-        {
+$(function (e) {
+    $('.card').each(function (i) {
+        $(this).delay(500 * (i + 1)).slideDown(500 * (1 + i), function () {
             $('nav.load').delay(500 * ($('.card').length) + 500).slideDown(5000);
         });
     });
